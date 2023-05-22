884cfa1b6067623cb682f48c57671b6d5e960aec
FlowerfulFort <skagyghk@naver.com>
2023-05-22 10:34:52 +0900

    Data_preparation

---
+++ README.md
@@ -0,0 +1,3 @@
+# 대전 공공자전거 타슈 데이터 분석
+## Data_Preparation.ipynb
+필요없는 데이터 제거
---
+++ getdata.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget https://www.djsiseol.or.kr/portal/open_data/2022.zip -P ./data/ --no-check-certificate
